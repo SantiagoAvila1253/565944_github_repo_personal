3eb70fa17bb16c2596bac244e4ab0e2f7b5ff601
santiagoavila1253 <savilagonzalez@uade.edu.ar>
2026-03-18 19:49:26 -0300

    Cambie la suma por multiplicación

--- repaso.py
+++ repaso.py
@@ -1,6 +1,6 @@
 num=int(input("Ingrese un numero: "))
 num2= int(input("Ingrese un numero: "))
 
-suma = num + num2
+multiplicar = num * num2
 
-print("La suma es: ", suma)
+print("La multiplicacion es: ", multiplicar)
